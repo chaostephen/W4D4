9d402ef966e0ec5873d6e77bbd8031f62b69158e
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-14 08:49:40 -0700

    second

--- Project_1/time_complexity.rb
+++ Project_1/time_complexity.rb
@@ -1,3 +1,17 @@
+# def my_min(list)
+#     list.sort[0]
+# end
+
+def my_min(list)
+    min=list[0]
+    list.each do |ele|
+        min=ele if ele<min
+    end
+    min
+end
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min(list)  # =>  -5
+
 
 # # possible sub-sums
 # [5]           # => 5
@@ -20,15 +34,24 @@
 # ```ruby
 # list = [-5, -1, -3]
 # largest_contiguous_subsum(list) # => -1 (from [-1])
+# def largest_contiguous_subsum(list)
+#     largest_sum=0
+#     list.each.with_index do |ele,idx|
+#         (idx+1...list.length).each do |jdx|
+#             largest_sum=ele + list[jdx] if largest_sum<ele + list[jdx]
+#         end
+#     end
+#     largest_sum
+# end
+
 def largest_contiguous_subsum(list)
-    largest_sum=0
-    list.each.with_index do |ele,idx|
-        (idx+1...list.length).each do |jdx|
-            largest_sum=ele + list[jdx] if largest_sum<ele + list[jdx]
-        end
-    end
-    largest_sum
+    # largest_sum=0
+    # first_ele=0
+    # last_ele=list.length-1
+    # while first_ele<last_ele
+    # end
 end
+
 
 list = [5, 3, -7]
 p largest_contiguous_subsum(list) # => 8
