af0be419b0387700d6ea837fdc26cf0c65d2d162
DispicableLee <lee.robert053@gmail.com>
2023-09-14 12:13:47 -0400

    done

--- Project_2/anagrams.rb
+++ Project_2/anagrams.rb
@@ -24,5 +24,67 @@ def anagram?(str1,str2)
     false
 end
 
-p anagram?("gizmo", "sally")    #=> false
-p anagram?("elvis", "lives")    #=> true
+
+# def anagram?(str1, str2)
+#     hash1 = {}
+#     hash2 = {}
+#     str1.each_char{|c| hash1[c] = str1.count(c)}
+#     str2.each_char{|c| hash2[c] = str2.count(c)}
+#     hash1 == hash2
+# end
+
+# p anagram?("gizmo", "sally")    #=> false
+# p anagram?("elvis", "lives")    #=> true
+
+
+
+
+def first_anagram(str, target)
+    permutations = str.chars.permutation.to_a
+    permutations.each do |pe| 
+        return true if pe.join("") == target
+    end
+    false
+end
+
+
+
+
+def second_anagram(str1, str2)
+    temp = str2.chars
+    str1.chars.each do |c|
+        found_idx = temp.index(c)
+        p "found_idx: #{found_idx}"
+        temp[found_idx] = ""
+    end 
+    temp.join("").length ==  0
+
+end
+
+def jumble_sort(str)
+    alphabet = ('a'..'z').to_a
+    sorted = false
+    until sorted
+        sorted = true
+        (0..str.length-2).each do |i|
+            if alphabet.index(str[i]) > alphabet.index(str[i+1])
+                str[i], str[i+1] = str[i+1], str[i]
+                sorted = false
+            end
+        end
+    end
+    str
+end
+
+
+def third_anagram(str1, str2)
+    jumble_sort(str1) == jumble_sort(str2)
+end
+
+
+
+
+p third_anagram("hello", "ohlel")
+
+
+# p jumble_sort("ohlel")
