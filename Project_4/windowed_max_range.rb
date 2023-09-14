6da3a787bcbfb006840d8f64e975f4a25a699e00
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-14 14:30:05 -0700

    stackqueue

--- Project_4/windowed_max_range.rb
+++ Project_4/windowed_max_range.rb
@@ -35,89 +35,102 @@ end
 
 class MyStack
     def initialize
-        @store = []
+        @in_stack = []
     end
 
     def empty?
-        @store.length==0
+        @in_stack.length==0
     end
 
     def pop
-        @store.pop
+        @in_stack.pop
     end
     def push(n)
-        @store<<n
+        @in_stack<<n
     end
     def size
-        @store.length
+        @in_stack.length
     end
     def peek(n)
-        @store[-n..-1]
+        @in_stack[-n..-1]
     end
 end
-
-def windowed_max_range_stackqueue(arr,window)
-    stack1 = MyStack.new
-    stack2 = MyStack.new
-    stack1.store = arr[0...window]
-    max_diff = 0
-    if stack1.store.max - stack1.store.min > max_diff
-        max_diff = stack1.store.max - stack1.store.min
+def stackqueue(arr)
+    hsh1=Hash.new(0)
+    min=arr[0]
+    max=arr[0]
+    arr.each.with_index do |ele,idx|
+        if idx !=0
+            min,max=hsh1[arr[idx-1]]
+            if ele>max
+                max=ele
+            else ele<min
+                min=ele
+            end
+        end
+        hsh1[ele]=min,max
     end
-    i = 1
-    until window+i < arr.length
-        stack2.push(stack1.pop)
-        stack2.push(arr[window+i])
-        i+=1
-    end
+    hsh1
 end
-
-
 class StackQueue
     def initialize
         @in_stack = MyStack.new
         @out_stack = MyStack.new
     end
-
+    
     def enqueue(n)
         @in_stack << n
     end
-
+    
     def flip
         until @in_stack.empty?
             @out_stack << @in_stack.pop
         end
     end
-
+    
     def dequeue
         if @out_stack.empty?
             flip
         end
         return @out_stack.pop
     end
-
+    
     def show
         @out_stack[0]
     end
-
+    
     def size
         @in_stack.size + @out_stack.size
     end
-
+    
     def empty?
         @in_stack.empty? && @out_stack.empty?
     end
-    
-
-    
-
-
-
-
-
 end
 
-p windowed_max_range_mystack([1, 0, 2, 5, 4, 8], 2) # 4 #4, 8
-p windowed_max_range_mystack([1, 0, 2, 5, 4, 8], 3) # 5 #0, 2, 5
-p windowed_max_range_mystack([1, 0, 2, 5, 4, 8], 4) # 6 #2, 5, 4, 8
-p windowed_max_range_mystack([1, 3, 2, 5, 4, 8], 5) # 6 #3, 2, 5, 4, 8
+def windowed_max_range_stackqueue(arr,window)
+    stack = StackQueue.new
+    stack.in_stack = arr[0...window]
+    max_diff = stackqueue(stack.in_stack)[-1][1]-stackqueue(stack.in_stack)[-1][0]
+    
+    
+    # i = 1
+    # until window+i < arr.length
+    #     stack.out_stack.push(stack.in_stack.pop)
+    #     stack.out_stack.push(arr[window+i])
+    #     i+=1
+    # end
+    if stackqueue(stack.in_stack)[-1][1]-stackqueue(stack.in_stack)[-1][0] > max_diff
+        max_diff = stackqueue(stack.in_stack)[-1][1]-stackqueue(stack.in_stack)[-1][0]
+    end
+end
+
+
+
+# p windowed_max_range_mystack([1, 0, 2, 5, 4, 8], 2) # 4 #4, 8
+# p windowed_max_range_mystack([1, 0, 2, 5, 4, 8], 3) # 5 #0, 2, 5
+# p windowed_max_range_mystack([1, 0, 2, 5, 4, 8], 4) # 6 #2, 5, 4, 8
+# p windowed_max_range_mystack([1, 3, 2, 5, 4, 8], 5) # 6 #3, 2, 5, 4, 8
+
+
+p stackqueue([1,2,3,4,5])
